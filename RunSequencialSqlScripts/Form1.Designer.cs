7bbcd7669c3a29b0f577686c6d0a3a3cc4cc69a9
Jeferson Silveira <jefersoncomp@gmail.com>
2024-03-20 15:46:11 -0300

    store RunSequencialScripts project
    Para conseguirmos executar uma grande quantidade de scripts sql que estavam em vários arquivos. Criei esse projeto que percorre os arquivos, conecta com o sql server e envia o comando para execução. No fim, marca o arquivo que foi executado com  a palavra ok.

---
+++ RunSequencialSqlScripts/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace RunSequencialSqlScripts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.btnTestarConexao = new System.Windows.Forms.Button();
+            this.txtBanco = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.txtPasta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInf = new System.Windows.Forms.Label();
+            this.lblComands = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(7, 41);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 0;
+            this.txtServidor.TextChanged += new System.EventHandler(this.txtServidor_TextChanged);
+            // 
+            // btnTestarConexao
+            // 
+            this.btnTestarConexao.Location = new System.Drawing.Point(7, 143);
+            this.btnTestarConexao.Name = "btnTestarConexao";
+            this.btnTestarConexao.Size = new System.Drawing.Size(232, 23);
+            this.btnTestarConexao.TabIndex = 4;
+            this.btnTestarConexao.Text = "Testar Conexão";
+            this.btnTestarConexao.UseVisualStyleBackColor = true;
+            this.btnTestarConexao.Click += new System.EventHandler(this.btnTestarConexao_Click);
+            // 
+            // txtBanco
+            // 
+            this.txtBanco.Location = new System.Drawing.Point(139, 41);
+            this.txtBanco.Name = "txtBanco";
+            this.txtBanco.Size = new System.Drawing.Size(100, 20);
+            this.txtBanco.TabIndex = 1;
+            this.txtBanco.TextChanged += new System.EventHandler(this.txtServidor_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Banco de dados";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(7, 96);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtServidor_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Usuário";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(139, 96);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.TextChanged += new System.EventHandler(this.txtServidor_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Senha";
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Enabled = false;
+            this.btnProcessar.Location = new System.Drawing.Point(19, 293);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(232, 23);
+            this.btnProcessar.TabIndex = 3;
+            this.btnProcessar.Text = "Processar";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // txtPasta
+            // 
+            this.txtPasta.Location = new System.Drawing.Point(19, 231);
+            this.txtPasta.Name = "txtPasta";
+            this.txtPasta.Size = new System.Drawing.Size(232, 20);
+            this.txtPasta.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Pasta dos scripts";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtServidor);
+            this.groupBox1.Controls.Add(this.btnTestarConexao);
+            this.groupBox1.Controls.Add(this.txtSenha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtBanco);
+            this.groupBox1.Controls.Add(this.txtUsuario);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 190);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parâmetros Conexão";
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(16, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(242, 36);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Faça backup dos scripts pois eles serão apagados a medida que forem sendo executa" +
+    "dos";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblComands);
+            this.panel1.Controls.Add(this.lblInf);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 322);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 38);
+            this.panel1.TabIndex = 13;
+            // 
+            // lblInf
+            // 
+            this.lblInf.AutoSize = true;
+            this.lblInf.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblInf.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblInf.Location = new System.Drawing.Point(0, 0);
+            this.lblInf.Name = "lblInf";
+            this.lblInf.Size = new System.Drawing.Size(16, 13);
+            this.lblInf.TabIndex = 13;
+            this.lblInf.Text = "...";
+            // 
+            // lblComands
+            // 
+            this.lblComands.AutoSize = true;
+            this.lblComands.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblComands.Location = new System.Drawing.Point(0, 25);
+            this.lblComands.Name = "lblComands";
+            this.lblComands.Size = new System.Drawing.Size(16, 13);
+            this.lblComands.TabIndex = 14;
+            this.lblComands.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 360);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPasta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnProcessar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Executor de Arquivos Scripts";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Button btnTestarConexao;
+        private System.Windows.Forms.TextBox txtBanco;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.TextBox txtPasta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblInf;
+        private System.Windows.Forms.Label lblComands;
+    }
+}
+
---
+++ RunSequencialSqlScripts/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RunSequencialSqlScripts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestarConexao_Click(object sender, EventArgs e)
+        {
+            if (ValidarDados())
+            {
+                if (ValidarConexao())
+                {
+                    MessageBox.Show(this, "Conexão feita com sucesso.");
+                    this.DesbloquearCampos();
+                }
+            }
+        }
+
+        private bool ValidarDados()
+        {
+            if (string.IsNullOrWhiteSpace(this.txtServidor.Text))
+            {
+                MessageBox.Show(this, "Informe o servidor");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(this.txtBanco.Text))
+            {
+                MessageBox.Show(this, "Informe o banco de dados");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(this.txtUsuario.Text))
+            {
+                MessageBox.Show(this, "Informe o usuário");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(this.txtSenha.Text))
+            {
+                MessageBox.Show(this, "Informe a senha");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidarConexao()
+        {
+            try
+            {
+                string conectionString = $"Server={this.txtServidor.Text};Database={this.txtBanco.Text};User Id={this.txtUsuario.Text};Password={this.txtSenha.Text};";
+                SqlConnection sqlConnection = new SqlConnection(conectionString);
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("Select 1 as Sucess", sqlConnection);
+                if (sqlCommand.ExecuteScalar() != null)
+                {
+                    return true;
+                }
+
+                MessageBox.Show(this, "Conexão não validada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Conexão não validada");
+                MessageBox.Show(this, ex.Message);
+            }
+
+            return false;
+        }
+
+        private void txtServidor_TextChanged(object sender, EventArgs e)
+        {
+            BloquearCampos();
+        }
+
+        private void DesbloquearCampos()
+        {
+            this.btnProcessar.Enabled = true;
+        }
+
+        private void BloquearCampos()
+        {
+            this.btnProcessar.Enabled = false;
+        }
+
+        private void btnProcessar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.txtPasta.Text) || !Directory.Exists(this.txtPasta.Text))
+            {
+                MessageBox.Show(this, "Pasta dos scripts não informada, ou não existe.");
+                return;
+            }
+
+            if (this.Processar())
+            {
+                MessageBox.Show(this, "Processamento finalizado!!!");
+            }
+            else
+            {
+                MessageBox.Show(this, "Não foi possível executar todos os scripts.");
+            }
+        }
+
+        private bool Processar()
+        {
+            DirectoryInfo df = new DirectoryInfo(this.txtPasta.Text);
+            var files = df.GetFiles("*.sql");
+            int totalArquivos = files.Count();
+            SetarTextoInf($"{totalArquivos} scripts serão processados.");
+            Application.DoEvents();
+            int count = 1;
+
+            foreach (var file in files)
+            {
+                SetarTextoInf($"Processando {count} de {totalArquivos} scripts...");
+                Application.DoEvents();
+                if (!ProcessarComandoTransaction(file))
+                {
+                    MessageBox.Show(this, "Não foi possível processar todos os scripts. O processamento será interrompido.");
+                    return false;
+                }
+
+                string novoNome = "OK_" + file.Name;
+                File.Move(file.FullName, file.FullName.Replace(file.Name, novoNome));
+
+                Application.DoEvents();
+                count++;
+            }
+
+            SetarTextoInf("");
+            return true;
+        }
+
+        private bool ProcessarComandoTransaction(FileInfo f)
+        {
+            SqlConnection sqlConnection = null;
+            SqlTransaction sqlTransaction = null;
+
+            try
+            {
+                string conectionString = $"Server={this.txtServidor.Text};Database={this.txtBanco.Text};User Id={this.txtUsuario.Text};Password={this.txtSenha.Text};";
+                sqlConnection = new SqlConnection(conectionString);
+                sqlConnection.Open();
+                string comandoTotal = File.ReadAllText(f.FullName);
+                sqlTransaction = sqlConnection.BeginTransaction();
+
+                var comandos = comandoTotal.Split(new string[] { "\r\nGO ", "\r\nGO\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                int totalcomandos = comandos.Length;
+                int countComandos = 1;
+             
+                foreach (var comando in comandos)
+                {
+                    SetarTextoComandInf($"executando {countComandos} de {comandos.Length} comandos");
+                    Application.DoEvents();
+                    if (!ProcessarComando(comando, sqlConnection, sqlTransaction))
+                    {
+                        throw new Exception("Um dos comandos contidos no strip " + f.FullName + " não pode ser processado com sucesso.");
+                    }
+
+                    countComandos++;
+                }
+
+                SetarTextoComandInf("");
+                sqlTransaction.Commit();
+                sqlConnection?.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Erro ao processar comando.");
+                MessageBox.Show(this, ex.Message);
+
+                Exception exError = ex;
+                StringBuilder sb = new StringBuilder(DateTime.Now.ToString("dd/MM/yyyy HH:ss" + Environment.NewLine));
+                while (exError != null)
+                {
+                    sb.AppendLine(exError.Message);
+                    exError = exError.InnerException;
+                }
+
+                File.AppendAllText("LogError.txt", sb.ToString());
+
+                sqlTransaction.Rollback();
+                sqlConnection?.Close();
+
+            }
+
+            return false;
+        }
+
+        private bool ProcessarComando(string comando, SqlConnection connection, SqlTransaction transaction)
+        {
+            SqlCommand sqlCommand = new SqlCommand(comando, connection, transaction);
+            sqlCommand.ExecuteNonQuery();
+            return true;
+        }
+
+        public void SetarTextoInf(string text)
+        {
+            if (lblInf.InvokeRequired)
+            {
+                // Call this same method but append THREAD2 to the text
+                Action safeWrite = delegate { SetarTextoInf(text); };
+                lblInf.Invoke(safeWrite);
+            }
+            else
+                lblInf.Text = text;
+        }
+
+        public void SetarTextoComandInf(string text)
+        {
+            if (lblComands.InvokeRequired)
+            {
+                // Call this same method but append THREAD2 to the text
+                Action safeWrite = delegate { SetarTextoInf(text); };
+                lblComands.Invoke(safeWrite);
+            }
+            else
+                lblComands.Text = text;
+        }
+    }
+}
